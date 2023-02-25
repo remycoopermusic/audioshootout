30aef5dd3e3a030f5c9ab87fa02a2744080e9bae
remycoopermusic <remyborsboom@hotmail.com>
2023-02-25 22:40:23 +0100

    chore: fixed the uploading screen transition to the contentview

--- Audio Shootout/Audio_ShootoutApp.swift
+++ Audio Shootout/Audio_ShootoutApp.swift
@@ -7,12 +7,24 @@
 
 import SwiftUI
 
+class AppState: ObservableObject {
+    @Published var isUploading: Bool = false
+}
+
+
 @main
 struct Audio_ShootoutApp: App {
+    @StateObject var fileManager = FileManager()
+    @StateObject var appState = AppState()
+
     var body: some Scene {
         WindowGroup {
-            UploadView()
-                .frame(minWidth: 600, minHeight: 400)
+            if appState.isUploading {
+                ContentView()
+            } else {
+                UploadView(fileManager: fileManager, isUploading: $appState.isUploading)
+                    .frame(minWidth: 600, minHeight: 400)
+            }
         }
     }
 }
--- Views/UploadView.swift
+++ Views/UploadView.swift
@@ -9,11 +9,15 @@ import SwiftUI
 import Combine
 
 struct UploadView: View {
-    @StateObject var fileManager = FileManager()
-    @State private var isUploading = false
+    @StateObject var fileManager: FileManager
+    @Binding var isUploading: Bool
     @State private var progress: Double = 0.0
 
-    
+    init(fileManager: FileManager, isUploading: Binding<Bool>) {
+        self._fileManager = StateObject(wrappedValue: fileManager)
+        self._isUploading = isUploading
+    }
+
     var body: some View {
         VStack {
             HStack {
@@ -26,9 +30,9 @@ struct UploadView: View {
                     .font(.system(size: 24))
             }
             .padding()
-            
+
             Spacer()
-            
+
             if fileManager.audioFiles.isEmpty {
                 VStack {
                     Image(systemName: "doc.on.clipboard.fill")
@@ -37,7 +41,7 @@ struct UploadView: View {
                         .font(.title)
                         .foregroundColor(.gray)
                         .padding(.top)
-                    
+
                 }
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
                 .onTapGesture {
@@ -46,15 +50,15 @@ struct UploadView: View {
                     }
                 }
                 .onDrop(of: ["public.file-url"], isTargeted: nil) { providers -> Bool in
-                                    guard let item = providers.first else { return false }
-                                    item.loadItem(forTypeIdentifier: "public.file-url", completionHandler: { (urlData, error) in
-                                        guard error == nil, let urlData = urlData as? Data, let url = URL(dataRepresentation: urlData, relativeTo: nil) else { return }
-                                        DispatchQueue.main.async {
-                                            fileManager.audioFiles.append(url)
-                                        }
-                                    })
-                                    return true
-                                }
+                    guard let item = providers.first else { return false }
+                    item.loadItem(forTypeIdentifier: "public.file-url", completionHandler: { (urlData, error) in
+                        guard error == nil, let urlData = urlData as? Data, let url = URL(dataRepresentation: urlData, relativeTo: nil) else { return }
+                        DispatchQueue.main.async {
+                            fileManager.audioFiles.append(url)
+                        }
+                    })
+                    return true
+                }
             } else {
                 // Show selected files
                 List {
@@ -67,7 +71,7 @@ struct UploadView: View {
                 }
                 .padding()
             }
-            
+
             Spacer()
         }
         .navigationTitle("")
@@ -80,40 +84,54 @@ struct UploadView: View {
             }
         )
     }
-    
+
     private func uploadFiles() {
         let totalBytes = fileManager.audioFiles.map({try! $0.resourceValues(forKeys: [.fileSizeKey]).fileSize!}).reduce(0, +)
         var uploadedBytes = 0
-        
+
         let progressPublisher = PassthroughSubject<Double, Never>()
         let cancellable = progressPublisher
             .throttle(for: 0.5, scheduler: RunLoop.main, latest: true)
             .sink(receiveValue: { progress in
                 self.progress = progress
             })
-        
+
+        let group = DispatchGroup()
+
         DispatchQueue.global(qos: .background).async {
             fileManager.audioFiles.forEach { file in
                 let data = try! Data(contentsOf: file)
                 // Simulating file upload
                 let dataLength = Double(data.count)
-                _ = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
+                let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
                 var uploaded = 0.0
-
-                for _ in 1...10 {
+                
+                group.enter()
+                
+                for i in 1...10 {
                     RunLoop.main.run(until: Date()+0.01)
                     uploaded += dataLength/10
                     uploadedBytes += Int(uploaded)
                     let uploadProgress = Double(uploadedBytes) / Double(totalBytes)
                     progressPublisher.send(uploadProgress)
+                    print("Uploaded \(i * 10)%")
                 }
+                
+                timer.upstream.connect().cancel()
+                group.leave()
             }
-            cancellable.cancel()
-            DispatchQueue.main.async {
-                isUploading = true
+            
+            group.notify(queue: .main) {
+                cancellable.cancel()
+                DispatchQueue.main.async {
+                    isUploading = true
+                    print("isUploading set to true")
+                }
             }
         }
     }
+
+
 }
 
 
@@ -121,6 +139,6 @@ struct UploadView: View {
 
 struct UploadView_Previews: PreviewProvider {
     static var previews: some View {
-        UploadView()
+        UploadView(fileManager: FileManager(), isUploading: .constant(false))
     }
 }
